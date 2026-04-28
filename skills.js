c1a940e9fff82fb6a29c21f73fc77d7fbdec5474
Oravecz Olivér László <oli.oravecz@gmail.com>
2026-04-28 13:35:03 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,13 @@
+function calculateNumbers(var 1, var 2) {
+    var sum = var 1 + var 2;
+    var difference = var 1 - var 2;
+    var product = var 1 * var 2;
+    var quotient = var 1 / var 2;
+
+    return {
+        sum: sum,
+        difference: difference,
+        product: product,
+        quotient: quotient
+    };
+}
